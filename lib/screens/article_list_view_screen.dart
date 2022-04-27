30dd55a7a3123cc6c6aeb54998a99f5ed325824d
Ahmed ElNeklawi <70581947+0127822@users.noreply.github.com>
2022-04-27 17:58:46 +0200

    Mazen (Rearrange )

--- lib/screens/article_list_view_screen.dart
+++ lib/screens/article_list_view_screen.dart
@@ -1,10 +1,14 @@
-import 'package:app/compenents/article_list.dart';
+import 'package:app/compenents/list_view.dart';
+import 'package:app/data/articles_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../compenents/tab.dart';
 
-class ArticleListScreen extends StatelessWidget {
-  const ArticleListScreen({Key? key}) : super(key: key);
+// ignore: must_be_immutable
+class ArticleListView extends StatelessWidget {
+  const ArticleListView({Key? key}) : super(key: key);
+
   TabBar get _tabBar => const TabBar(
         indicatorColor: Color(0xFFBA0C2F),
         tabs: [
@@ -16,37 +20,31 @@ class ArticleListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final articlesProvider = Provider.of<ArticlesProvider>(context);
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
-          bottom: PreferredSize(
-            preferredSize: _tabBar.preferredSize,
-            child: ColoredBox(
-              color: Colors.black,
-              child: _tabBar,
-            ),
-          ),
-          elevation: 0,
-          leadingWidth: 0,
-          centerTitle: false,
-          backgroundColor: Colors.black,
-          title: Container(
-            padding: const EdgeInsets.only(top: 15.0),
-            child: const Text(
+            bottom: _tabBar,
+            backgroundColor: Colors.black,
+            centerTitle: false,
+            title: const Text(
               "Articles",
               style: TextStyle(
                   fontSize: 28,
                   color: Color(0xFFBA0C2F),
                   fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
+            )),
         body: TabBarView(children: [
-          ArticleListView(),
-          ArticleListView(),
-          ArticleListView(),
+          IeeeListView(
+            items: articlesProvider.articles,
+          ),
+          IeeeListView(
+            items: articlesProvider.articles,
+          ),
+          IeeeListView(
+            items: articlesProvider.articles,
+          ),
         ]),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,56 +1,78 @@
 import 'package:app/compenents/side_bar.dart';
+import 'package:app/data/ui_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../compenents/home_text.dart';
 
-class MyHomePage extends StatelessWidget {
+// ignore: must_be_immutable
+class MyHomePage extends StatefulWidget {
   const MyHomePage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Center(
-            child: Container(
-              padding: const EdgeInsets.only(left: 50.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    HomeText(
-                      title: "I E E E",
-                    ),
-                    HomeText(
-                      title: "S S C S",
-                      color: Color(0xffBA1B1B),
-                    ),
-                    HomeText(
-                      title: "Alex S C",
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    HomeText(
-                      title: "Providing the future electronics enginners",
-                      size: 12,
-                      color: Colors.grey,
-                      weight: FontWeight.normal,
-                    ),
-                    HomeText(
-                      title: "with the knowledge and skills they need",
-                      size: 12,
-                      color: Colors.grey,
-                      weight: FontWeight.normal,
-                    )
-                  ]),
+    final uiProvider = Provider.of<UiProvider>(context);
+    final sideBarStatus = uiProvider.isClicked;
+    return WillPopScope(
+      onWillPop: () async {
+        if (sideBarStatus == true) {
+          uiProvider.toggleSideBar();
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: GestureDetector(
+        onTap: () {
+          uiProvider.toggleSideBar();
+        },
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                padding: const EdgeInsets.only(left: 50.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      HomeText(
+                        title: "I E E E",
+                      ),
+                      HomeText(
+                        title: "S S C S",
+                        color: Color(0xffBA1B1B),
+                      ),
+                      HomeText(
+                        title: "Alex S C",
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      HomeText(
+                        title: "Providing the future electronics enginners",
+                        size: 12,
+                        color: Colors.grey,
+                        weight: FontWeight.normal,
+                      ),
+                      HomeText(
+                        title: "with the knowledge and skills they need",
+                        size: 12,
+                        color: Colors.grey,
+                        weight: FontWeight.normal,
+                      )
+                    ]),
+              ),
             ),
-          ),
-          const SideBar(),
-        ],
+            const SideBar(),
+          ],
+        ),
       ),
     );
   }
